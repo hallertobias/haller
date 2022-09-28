80355e70bfefb7602f2c5476c3b7b3e226c51ea1
hallertobias <haller.tobias@outlook.com>
2022-09-28 16:16:09 +0200

    Ändere aba.cpp um, benutzt nun fork() und sleeptime() um BA 6x mal auszugeben

--- src/aba.cpp
+++ src/aba.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
+#include <unistd.h> // fork
+#include <chrono>
+#include <thread>
+#include <csignal>
 
 using namespace std;
 
 int main() {
-    cout << "Hello world!" << endl;
+    auto pid{fork()};
+    int counter = 0;
+    char c{};
+    chrono::milliseconds sleeptime(500);
+    if (pid == 0) {
+        c = 'A';
+        }
+    if (pid != 0) {
+        c = 'B';
+    }
+    while (true) {
+        if (pid != 0 && counter == 6) {
+            cout << endl;
+            kill(pid, SIGKILL);
+            exit(EXIT_SUCCESS);
+        }
+        cout << c << flush;
+        this_thread::sleep_for(sleeptime);
+        counter++;
+    }
 }
