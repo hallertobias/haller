0b30d1a1e39585680027f8b883f5ccf689ca29be
hallertobias <haller.tobias@outlook.com>
2022-10-11 17:26:35 +0200

    Ausgabe um Sekundenanzeige erweitert und maximal 10 Runden.

--- 02_threads/src/car.cpp
+++ 02_threads/src/car.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "car.h"
 #include <string>
+#include <random>
 
 using namespace std;
 
@@ -9,11 +10,19 @@ Car::Car(string name) {
 }
 
 void Car::printOut() {
-    int i{};
-    while(true) {
-        cout << i << ". Runde " << this->carName << endl;
+    int i{1};
+    string output;
+    random_device rd;
+    mt19937 gen{rd()};
+    uniform_real_distribution<> dis{1, 10};
+    while(i <= 10) {
+        int time = dis(gen);
+        double rounded = ceil(dis(gen)*100.00)/100.00;
+        output = to_string(i) + ". Runde " + this->carName
+         + " (" + to_string(rounded).substr(0, 4) + "s)" + "\n";
+        cout << output << flush;
         i++;
-        this_thread::sleep_for(1s);
+        this_thread::sleep_for(chrono::milliseconds{time}*1000);
     }
 }
 
--- 02_threads/src/contest.cpp
+++ 02_threads/src/contest.cpp
@@ -7,7 +7,13 @@ using namespace std;
 int main() {
     Car c{"BWM X5 M"};
     thread t{c};
+    Car c2{"AUDI RS6"};
+    thread t2{c2};
+    Car c3{"AMG C63"};
+    thread t3{c3};
     t.join();
+    t2.join();
+    t3.join();
     return 0;
 }
 
