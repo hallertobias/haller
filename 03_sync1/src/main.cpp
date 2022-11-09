200a326e892e26b8c0304631ae9dc26837833ca5
hallertobias <haller.tobias@outlook.com>
2022-11-09 14:34:53 +0100

    Füge CLI11 hinzu

--- 03_sync1/src/main.cpp
+++ 03_sync1/src/main.cpp
@@ -1,4 +1,5 @@
 #include "account.cpp"
+#include "account.h"
 #include <thread>
 #include <iostream>
 
@@ -9,14 +10,25 @@ int main() {
     /* acc.desposit(15);  /// Punkt 1
     cout << acc.withdraw(6) << endl;
     cout << acc.withdraw(10) << endl; */
-    acc.desposit(1);
+    /* acc.desposit(1);
     thread t1([&](){
         cout << acc.withdraw(1) << endl;
     }); 
     thread t2([&](){
         cout << acc.withdraw(1) << endl;
-    }); 
+    });  */
+    /* Depositer d1{acc, 5};
+    Depositer d2{acc, 5};
+    thread t1{ref(d1)};
+    thread t2{ref(d2)};
     t1.join();
     t2.join();
+    cout << acc.get_balance() << endl; */
+    CLI::App app("Account app");
+    int balance{0};
+    app.add_option("balance", balance, "Initial balance")->required();
+    int deposits{5};
+    app.add_option("-d,--deposits", deposits, "Count of deposits")->->capture_default_str();
+    CLI11_PARSE(app, argc, argv)
     return 0;
 }
