d22d7cc905ffd537f58f6163afeee5ab8ce2737a
hallertobias <haller.tobias@outlook.com>
2022-11-09 14:34:31 +0100

    Füge Unique Lock hinzu

--- 03_sync1/src/account.cpp
+++ 03_sync1/src/account.cpp
@@ -1,17 +1,22 @@
 #include "account.h"
 #include <thread>
+#include <mutex>
 
 using namespace std;
+
+mutex m;
 
 int Account::get_balance() {
     return this->balance;
 }
 
 void Account::desposit(int amount) {
+    unique_lock<mutex> ul{m};
     this->balance += amount;
 }
 
 bool Account::withdraw(int amount) {
+    lock_guard<mutex> guard{m};
     if ((this->balance - amount) >= 0) {
         this_thread::yield();
         this->balance -= amount;
