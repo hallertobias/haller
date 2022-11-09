38ced470f3ab7acc72b741dbf8520c53f1549a45
hallertobias <haller.tobias@outlook.com>
2022-11-09 17:07:34 +0100

    Fertig gestellt

--- 03_sync1/src/main.cpp
+++ 03_sync1/src/main.cpp
@@ -1,3 +1,10 @@
+/*
+    author: Haller Tobias
+    date:   09.11.2022
+    desc:   03_syn1
+*/
+
+#include "CLI11.hpp"
 #include "account.cpp"
 #include "account.h"
 #include <thread>
@@ -5,8 +12,7 @@
 
 using namespace std;
 
-int main() {
-    Account acc{};
+int main(int argc, char* argv[]) {
     /* acc.desposit(15);  /// Punkt 1
     cout << acc.withdraw(6) << endl;
     cout << acc.withdraw(10) << endl; */
@@ -17,18 +23,21 @@ int main() {
     thread t2([&](){
         cout << acc.withdraw(1) << endl;
     });  */
-    /* Depositer d1{acc, 5};
-    Depositer d2{acc, 5};
-    thread t1{ref(d1)};
-    thread t2{ref(d2)};
-    t1.join();
-    t2.join();
-    cout << acc.get_balance() << endl; */
+
     CLI::App app("Account app");
     int balance{0};
     app.add_option("balance", balance, "Initial balance")->required();
     int deposits{5};
-    app.add_option("-d,--deposits", deposits, "Count of deposits")->->capture_default_str();
+    app.add_option("-d,--deposits", deposits, "Count of deposits")->capture_default_str();
     CLI11_PARSE(app, argc, argv)
+
+    Account acc{balance};
+    Depositer d1{acc, deposits};
+    Depositer d2{acc, deposits};
+    thread t1{ref(d1)};
+    thread t2{ref(d2)};
+    t1.join();
+    t2.join();
+    cout << acc.get_balance() << endl; 
     return 0;
 }
