501044d0ea9ecbdaca6b77e0b9615dc7f548f140
hallertobias <haller.tobias@outlook.com>
2022-12-07 13:00:31 +0100

    Füge Semaphore hinzu

---
+++ 05_sync3/include/semaphore.h
@@ -0,0 +1,22 @@
+/*
+    author: Haller Tobias, 5AHIF
+*/
+
+#pragma once
+
+#include <mutex>
+#include <atomic>
+#include <condition_variable>
+
+class Semaphore{
+    private:
+        int cnr;
+        std::mutex mtx;
+        std::condition_variable wcv;
+    public:
+        Semaphore() : cnr{0}{};
+        Semaphore(int size) : cnr{size}{};
+        void acquire();
+        void release();
+        int available_permits();
+}; 
---
+++ 05_sync3/src/semaphore.cpp
@@ -0,0 +1,27 @@
+/*
+    author: Haller Tobias, 5AHIF
+*/
+
+#include "semaphore.h"
+
+#include <mutex>
+
+using namespace std;
+
+void Semaphore::release(){
+    cnr += 1;
+    wcv.notify_one();
+}
+
+void Semaphore::acquire(){
+    unique_lock<mutex> ul{mtx};
+    wcv.wait(ul, [&]() {
+        return (cnr - 1) >= 0;
+    });
+
+    cnr -= 1;
+}
+
+int Semaphore::available_permits(){
+    return cnr;
+}
