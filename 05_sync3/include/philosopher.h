b74a6828958a20c106e61201f7b3b27148f9c1e1
hallertobias <haller.tobias@outlook.com>
2022-12-07 12:33:44 +0100

    Füge Philosophen hinzu

---
+++ 05_sync3/include/philosopher.h
@@ -0,0 +1,23 @@
+/*
+    author: Haller Tobias
+    class: 5AHIF
+*/
+
+#include <thread>
+#include <mutex>
+
+using namespace std;
+
+class Philosopher {
+    private: 
+        int id; 
+        mutex& leftFork; 
+        mutex& rightFork;
+    public:
+        Philosopher(int id, std::mutex& leftFork, std::mutex& rightFork) : 
+          id(id),
+          leftFork(leftFork),
+          rightFork(rightFork)
+          {};
+        void operator()();
+};
