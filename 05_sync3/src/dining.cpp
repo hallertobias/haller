b2344fe1d9cdec0f5a4e118a339df5c44d2f830b
hallertobias <haller.tobias@outlook.com>
2022-12-07 13:03:35 +0100

    Füge 2 weitere Philosphen hinzu

--- 05_sync3/src/dining.cpp
+++ 05_sync3/src/dining.cpp
@@ -11,12 +11,18 @@ int main() {
     Philosopher p1(1, mutexRight, mutexLeft);
     Philosopher p2(2, mutexRight, mutexLeft);
     Philosopher p3(3, mutexRight, mutexLeft);
+    Philosopher p4(4, mutexRight, mutexLeft);
+    Philosopher p5(5, mutexRight, mutexLeft);
 
     thread t1{p1};
     thread t2{p2};
     thread t3{p3};
+    thread t4{p4};
+    thread t5{p5};
 
     t1.join();
     t2.join();
     t3.join();
+    t4.join();
+    t5.join();
 }
