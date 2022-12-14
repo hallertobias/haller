7f746143c3abd003b8ad4dfc86b48f1f13eb9dfc
hallertobias <haller.tobias@outlook.com>
2022-12-14 13:13:22 +0100

    Bugfix in 05_sync3

--- 05_sync3/src/philosopher.cpp
+++ 05_sync3/src/philosopher.cpp
@@ -24,34 +24,49 @@ void println(const T& word, const Rest&... rest) {
     println(rest...);
 }
 
-void Philosopher::operator()(SemaphoreOwn* s) {
-    while(true){
-        println("Philosopher ", id, " is thinking...");
+void Philosopher::operator()(SemaphoreOwn* s, bool livelock) {
+    while (true){
+        println("Philosopher", id, "is thinking...");
         this_thread::sleep_for(1s);
 
-        if (s != nullptr) {
+        println("Philosopher", id, "attempts to get left fork");
+        if(s != nullptr){
             s->acquire();
         }
-
-        println("Philosopher ", id, " attempts to get left fork");
         leftFork.lock();
+        println("Philosopher", id, "got left fork. Now he wants the right one...");
 
-        println("Philosopher ", id, " got left fork. Now he wants the right one... ");
         this_thread::sleep_for(5s);
-        println("Philosopher ", id, " attempts to get right fork");
-        rightFork.lock();
 
-        println("Philosopher ", id, " got right fork. Now he is eating...");
+        println("Philosopher", id, "attempts to get right fork");
+        if (livelock){
+            while(!rightFork.try_lock_for(chrono::seconds(3))) {
+                this_thread::sleep_for(100ms);
+
+                if (s != nullptr) {
+                    s->release();
+                }
+
+                leftFork.unlock();
+                println("Philosopher", id, "released left fork due to timeout getting the right one!");
+            }
+            this_thread::sleep_for(3s);
+            continue;
+        } else {
+            rightFork.lock();
+        }
+
+        println("Philosopher", id, "got right fork. Now he is eating...");
         this_thread::sleep_for(2s);
+        println("Philosopher", id, "finished eating");
 
-        println("Philosopher ", id, " finished eating");
-        if (s != nullptr){
+        if(s != nullptr){
             s->release();
         }
         leftFork.unlock();
-        println("Philosopher", id, " released left fork");
-
+        println("Philosopher", id, "released left fork");
+        println("currently", s->available_permits(), "left forks available");
         rightFork.unlock();
-        println("Philosopher ", id, " released right fork");;
+        println("Philosopher", id, "released right fork");
     }
 };
