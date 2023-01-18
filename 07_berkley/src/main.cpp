b57d7006a31288d19677f2302ba2c0bb00f3e6d4
hallertobias <haller.tobias@outlook.com>
2023-01-18 20:03:55 +0100

    Teste Channels

--- 07_berkley/src/main.cpp
+++ 07_berkley/src/main.cpp
@@ -25,7 +25,12 @@ class TimeSlave {
             this->name = name;
         };
         void operator()() {
-            clock();
+            long value;
+            Pipe<long>& pipe = this->channel->getPipe1();
+            while (pipe >> value) {
+                std::cout << name << ": " << value << std::endl;
+            }
+            std::cout << name << ": end" << std::endl;
         };
         Channel* getChannel() {
             return channel;
@@ -43,7 +48,14 @@ class TimeMaster {
             this->name = name;
         }
         void operator()() {
-            clock();
+            for (int i = 0; i < 3; i++) {
+            this->channel1->getPipe1() << i;
+            this->channel2->getPipe2() << i;
+            }
+            std::this_thread::sleep_for(std::chrono::milliseconds(500));
+            this->channel1->getPipe1().close();
+            this->channel2->getPipe2().close();
+            std::cout << "master: end" << std::endl;
         };
         void setChannel1(Channel* channel) {
             this->channel1 = channel;
@@ -54,8 +66,17 @@ class TimeMaster {
 };
 
 int main() {
-    thread t(TimeSlave{"Sklave 1", 12, 12, 12});
-    t.join();
-    thread t2(TimeSlave{"Sklave 2", 12, 12, 12});
-    t2.join();
+    TimeMaster master{"Test Meister", 12, 12, 12};
+    thread masterThread{ref(master)};
+    TimeSlave slave1{"Test Sklave 1", 11, 12, 12};
+    thread slave1Thread{ref(slave1)};
+    TimeSlave slave2{"Test Sklave 2", 10, 12, 12};
+    thread slave2Thread{ref(slave2)};
+
+    master.setChannel1(slave1.getChannel());
+    master.setChannel2(slave2.getChannel());
+
+    slave1Thread.join();
+    slave2Thread.join();
+    masterThread.join(); 
 } 
