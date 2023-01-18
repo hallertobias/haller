e26da5d3061eac3b761222831e75c0f853688b16
hallertobias <haller.tobias@outlook.com>
2023-01-18 17:16:22 +0100

    Verschönere Code

--- 07_berkley/include/clock.h
+++ 07_berkley/include/clock.h
@@ -6,11 +6,11 @@
 using namespace std;
 
 class Clock {
-public:
-    Clock(string name);
-    Clock(string name_, int hours_, int minutes_, int seconds_);
-    void operator()();
-private:
-    string name;
-    chrono::time_point<std::chrono::system_clock> curr_time;
+    private:
+        string name;
+        chrono::time_point<chrono::system_clock> curr_time = chrono::system_clock::now();
+    public:
+        Clock(string name);
+        Clock(string name_, int hours_, int minutes_, int seconds_);
+        void operator()();
 };
