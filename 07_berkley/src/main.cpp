e9570d0b57dbcb4a99d4abe4705801a7050c5f76
hallertobias <haller.tobias@outlook.com>
2023-01-19 14:59:16 +0100

    Füge Berkley Algo hinzu

--- 07_berkley/src/main.cpp
+++ 07_berkley/src/main.cpp
@@ -19,13 +19,21 @@ class TimeSlave {
     private:
         string name;
         Clock clock;
-        Channel* channel{new Channel()};
+        Channel* channel = new Channel();
     public:
         TimeSlave(string name, int hours, int minutes, int seconds) : clock(name + " Clock", hours, minutes, seconds) {
             this->name = name;
         };
         void operator()() {
-            clock();
+            long syncTime;
+            while(this->channel->getPipe1() >> syncTime) {
+                if (syncTime > 0) {
+                    this->clock.from_time(syncTime);
+                } else {
+                    cout << this->name + "Slave request time: " + to_string(this->clock.to_time()) << endl;
+                    this->channel->getPipe2() << this->clock.to_time(); 
+                }
+            }
         };
         Channel* getChannel() {
             return channel;
@@ -43,7 +51,10 @@ class TimeMaster {
             this->name = name;
         }
         void operator()() {
-            clock();
+            while(true) {
+                doBerkley();
+                this_thread::sleep_for(10s); 
+            }
         };
         void setChannel1(Channel* channel) {
             this->channel1 = channel;
@@ -51,6 +62,28 @@ class TimeMaster {
         void setChannel2(Channel* channel) {
             this->channel2 = channel;
         };
+        void doBerkley() {
+            long slave1Time;
+            long slave2Time;
+            long time; 
+
+            cout << "Starting berkley algo with: " + to_string(this->clock.to_time()) << endl;
+
+            this->channel1->getPipe1() << -1;
+            this->channel2->getPipe2() << -1;
+
+            time = this->clock.to_time();
+
+            this->channel1->getPipe2() << slave1Time;
+            this->channel2->getPipe2() << slave2Time;
+
+            long syncTime = (time + slave1Time + slave2Time) / 3;
+
+            this->clock.from_time(syncTime);
+            this->channel1->getPipe1() << syncTime;
+            this->channel2->getPipe2() << syncTime;
+
+        }
 };
 
 int main() {
