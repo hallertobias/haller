f607887192946d98c2f2f660df1a405f963593ea
hallertobias <haller.tobias@outlook.com>
2023-01-24 11:25:41 +0100

    Füge Verbindung zum Daytime Server hinzu

--- 08_daytime/src/daytime_client/main.cpp
+++ 08_daytime/src/daytime_client/main.cpp
@@ -1,7 +1,22 @@
 #include "spdlog/spdlog.h"
 #include "spdlog/sinks/stdout_color_sinks.h"
+#include "asio.hpp"
+#include <iostream>
+
+using namespace std;
+using namespace asio::ip;
 
 int main() {
     auto logger = spdlog::stderr_color_mt("blue");
-    logger->info("sjdjf");
+
+    tcp::iostream stream("localhost", "1113");
+
+    if (stream) {
+        string line;
+        getline(stream, line);
+        logger->info(line);
+        cout << line << endl;
+    } else {
+        cout << "No connection to the server" << endl;
+    }
 }
