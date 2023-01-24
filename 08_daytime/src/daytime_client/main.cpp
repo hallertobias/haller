5e073fe4f213d878466591bec977e134edc27572
hallertobias <haller.tobias@outlook.com>
2023-01-24 17:57:41 +0100

    Füge variablen Port bei Client hinzu

--- 08_daytime/src/daytime_client/main.cpp
+++ 08_daytime/src/daytime_client/main.cpp
@@ -1,12 +1,20 @@
 #include "spdlog/spdlog.h"
 #include "asio.hpp"
 #include <iostream>
+#include "CLI11.hpp"
 
 using namespace std;
 using namespace asio::ip;
 
-int main() {
-    tcp::iostream stream("localhost", "1113");
+int main(int argc, char* argv[]) {
+    CLI::App app{"Daytime Client"};
+
+    unsigned short port = 1113;
+    app.add_option("-p, --port", port, "port to connect to");
+
+    CLI11_PARSE(app, argc, argv);
+
+    tcp::iostream stream("localhost", to_string(port));
 
     if (stream) {
         string line;
